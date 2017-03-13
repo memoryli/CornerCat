f9e80a6267c9e1b3b68689a11b4c0d2426ae3027
hicoldcat <hicoldcat@foxmail.com>
2017-03-13 22:10:22 +0800

    add reset.css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,16 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/Reset.css')
+// require('styles/App.css');
+
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+          Hello
       </div>
     );
   }
