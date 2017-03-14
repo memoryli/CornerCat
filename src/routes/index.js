075e1053e7acd2c30a3ba3b61d6cb943db5cec81
hicoldcat <hicoldcat@foxmail.com>
2017-03-14 22:52:21 +0800

    Modify server port and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,25 @@
 import MainView from 'views/MainView'
 import FooView from 'views/FooView'
 import BarView from 'views/BarView'
+import PhotoView from 'views/PhotoView'
+import TravelView from 'views/TravelView'
 
 export default [
   {
     path: '/',
+    component: MainView
+  },
+  {
+    path: '/home',
     component: MainView,
     childRoutes: [
       {
-        path: '/foo',
-        component: FooView
+        path: '/photo',
+        component: PhotoView
       },
       {
-        path: '/bar',
-        component: BarView
+        path: '/travel',
+        component: TravelView
       }
     ]
   }
--- src/views/MainView.jsx
+++ src/views/MainView.jsx
@@ -3,17 +3,21 @@ import { renderRoutes } from 'react-router-config'
 import { NavLink as Link } from 'react-router-dom'
 import './MainView.css'
 
+import NavBar from 'components/NavBar'
+
 export default class MainView extends Component {
   static propTypes = {
     route: PropTypes.object
   }
+
   render () {
     return (
       <div className='main-view'>
-        <div className='btn-group'>
-          <Link to='/foo' className='btn' activeClassName='active'>To Foo</Link>
-          <Link to='/bar' className='btn' activeClassName='active'>To Bar</Link>
-        </div>
+        // <div className='btn-group'>
+        //   <Link to='/foo' className='btn' activeClassName='active'>To Foo</Link>
+        //   <Link to='/bar' className='btn' activeClassName='active'>To Bar</Link>
+        // </div>
+        <NavBar/>
         <div className='view'>
           {renderRoutes(this.props.route.childRoutes)}
         </div>
