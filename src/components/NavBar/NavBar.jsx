3483efbbeadc11b3d6e026fb9ed71443e2af2070
hicoldcat <hicoldcat@foxmail.com>
2017-04-16 17:22:53 +0800

    wait to next component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -19,19 +19,19 @@ export default class NavBar extends Component {
     this.setState({
       current: e.key,
     });
-    history.push('/' + e.key)
+    // history.push('/' + e.key)
   }
   render() {
     return (
       <div className="navbar">
       <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal" defaultSelectedKeys={['home']}>
         <Menu.Item key="home">首页</Menu.Item>
-        <Menu.Item key="portfolio">作品</Menu.Item>
-        <Menu.Item key="traveler">行者</Menu.Item>
-        <Menu.Item key="essay">随笔</Menu.Item>
-        <Menu.Item key="design">设计</Menu.Item>
-        <Menu.Item key="gift">好物</Menu.Item>
-        <Menu.Item key="about">关于</Menu.Item>
+        <Menu.Item key="portfolio" disabled>作品</Menu.Item>
+        <Menu.Item key="traveler" disabled>行者</Menu.Item>
+        <Menu.Item key="essay" disabled>随笔</Menu.Item>
+        <Menu.Item key="design" disabled>设计</Menu.Item>
+        <Menu.Item key="gift" disabled>好物</Menu.Item>
+        <Menu.Item key="foo" disabled>关于</Menu.Item>
       </Menu>
     </div>
     );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,9 @@
 import MainView from 'views/MainView/MainView'
-import FooView from 'views/FooView'
-import BarView from 'views/BarView'
 
 export default [
   {
     path: '/',
     component: MainView,
-    childRoutes: [
-      {
-        path: '/foo',
-        component: FooView
-      },
-      {
-        path: '/bar',
-        component: BarView
-      }
-    ]
+    childRoutes: []
   }
 ]
