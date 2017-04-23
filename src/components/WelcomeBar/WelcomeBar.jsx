27d5d9efa71048a35115d7d9839be6e71f0e809b
hicoldcat <hicoldcat@foxmail.com>
2017-04-23 11:56:04 +0800

    modify welecomebar style

--- src/components/WelcomeBar/WelcomeBar.jsx
+++ src/components/WelcomeBar/WelcomeBar.jsx
@@ -5,9 +5,11 @@ export default class WelcomeBar extends Component {
   render () {
     return (
       <div className="welcome-bar">
-        <h1>Remember</h1>
-        <h1>your</h1>
-        <h1>dreams</h1>
+        <div className="content">
+          <p>Remember</p>
+          <p>your</p>
+          <p>dreams</p>
+        </div>
       </div>
     );
   }
