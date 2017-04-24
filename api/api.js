d987c196e175bb3fe86d1c96fa93f7340127373a
hicoldcat <hicoldcat@foxmail.com>
2017-04-24 12:05:53 +0800

    add mockdata

---
+++ api/api.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var apiRoutes = express.Router()
+
+// var portfolio = require('./mockdata/portfolio.json')
+// var traveler = require('./mockdata/traveler.json')
+// var essay = require('./mockdata/essay.json')
+// var design = require('./mockdata/design.json')
+// var gift = require('./mockdata/gift.json')
+// var about = require('./mockdata/about.json')
