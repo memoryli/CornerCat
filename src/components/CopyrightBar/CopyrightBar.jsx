0abe07bdbcc1bf65db18ad4e94f88ba9e5704432
hicoldcat <hicoldcat@foxmail.com>
2017-04-25 12:50:53 +0800

    add copyright bar component

---
+++ src/components/CopyrightBar/CopyrightBar.jsx
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react'
+import './CopyrightBar.less'
+
+export default class CopyrightBar extends Component {
+  render() {
+    return (
+      <div className="copyright-bar">
+        <p className="copyright">Copyright &copy;2017 <a className="weburl" target="_blank" href="http://www.forbetter.me">forbetter.me</a>&nbsp;&copy;All Rights Reserved.&nbsp;
+         <a href="http://www.miitbeian.gov.cn/" target="_blank">豫ICP备16007426号-1</a></p>
+        <p></p>
+    </div>
+    );
+  }
+}
