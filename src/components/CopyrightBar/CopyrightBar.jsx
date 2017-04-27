f1766f4af3f33b2db9e23939db1679b8dc577f48
hicoldcat <hicoldcat@foxmail.com>
2017-04-27 14:27:56 +0800

    modify copyright style

--- src/components/CopyrightBar/CopyrightBar.jsx
+++ src/components/CopyrightBar/CopyrightBar.jsx
@@ -5,7 +5,7 @@ export default class CopyrightBar extends Component {
   render() {
     return (
       <div className="copyright-bar">
-        <p className="copyright">Copyright &copy;2017 <a className="weburl" target="_blank" href="http://www.forbetter.me">forbetter.me</a>&nbsp;&copy;All Rights Reserved.&nbsp;
+        <p className="copyright">Copyright &copy;2017 <a className="weburl" target="_blank" href="http://www.forbetter.me">forbetter.me</a>&nbsp;All Rights Reserved.&nbsp;
          <a href="http://www.miitbeian.gov.cn/" target="_blank">豫ICP备16007426号-1</a></p>
         <p></p>
     </div>
