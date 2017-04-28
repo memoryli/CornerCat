892b315a3b48f322d63a9bcb707f1b26cf3e8402
hicoldcat <hicoldcat@foxmail.com>
2017-04-28 16:11:38 +0800

    modify router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,8 @@
-import MainView from 'views/MainView/MainView'
-
-export default [
-  {
-    path: '/',
-    component: MainView,
-    childRoutes: []
-  }
-]
+// Export Views
+export const MainView = require('views/MainView/MainView')
+export const PortfolioView = require('views/PortfolioView/PortfolioView')
+export const TravelerView = require('views/TravelerView/TravelerView')
+export const EssayView = require('views/EssayView/EssayView')
+export const DesignView = require('views/DesignView/DesignView')
+export const GiftView = require('views/GiftView/GiftView')
+export const AboutView = require('views/AboutView/AboutView')
