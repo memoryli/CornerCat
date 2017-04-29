a33c47e3c45825508ebd700d78469ba252b53140
hicoldcat <hicoldcat@foxmail.com>
2017-04-29 16:19:15 +0800

    complete MainView

--- src/views/MainView/MainView.jsx
+++ src/views/MainView/MainView.jsx
@@ -1,26 +1,19 @@
 import React, { Component, PropTypes } from 'react'
-import { renderRoutes } from 'react-router-config'
-import { NavLink as Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './MainView.css'
 
-import NavBar from 'components/NavBar/NavBar'
 import WelcomeBar from 'components/WelcomeBar/WelcomeBar'
 import CopyrightBar from 'components/CopyrightBar/CopyrightBar'
 
 export default class MainView extends Component {
-  static propTypes = {
-    route: PropTypes.object
-  }
   render () {
     return (
-      <div className='main-view main-bg'>
-        <NavBar />
-        <WelcomeBar />
-        <div className='view'>
-          {renderRoutes(this.props.route.childRoutes)}
+      <Router>
+        <div className='main-view main-bg'>
+          <WelcomeBar />
+          <CopyrightBar/>
         </div>
-        <CopyrightBar/>
-      </div>
-    )
+      </Router>
+    );
   }
 }
