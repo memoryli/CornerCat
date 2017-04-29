2930d68c2ce678b7e58722bd76261dd51042e835
hicoldcat <hicoldcat@foxmail.com>
2017-04-29 16:23:36 +0800

    app.jsx use route to instead of renderRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
 import { renderRoutes } from 'react-router-config'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router ,Route, Link } from 'react-router-dom'
 import routes from 'routes'
 import './App.css'
+import NavBar from 'components/NavBar/NavBar'
+
+import * as Views from './routes'
+
 
 export default class App extends Component {
   render () {
+    console.info(Views.MainView)
     return (
-      <div className='App'>
-        <Router>
-          {renderRoutes(routes)}
-        </Router>
-      </div>
+      <Router>
+        <div className='App'>
+          <NavBar />
+          <Route path="/home" component={Views.MainView.default}/>
+          <Route path="/portfolio" component={Views.PortfolioView.default}/>
+          <Route path="/traveler" component={Views.TravelerView.default}/>
+          <Route path="/essay" component={Views.EssayView.default}/>
+          <Route path="/design" component={Views.DesignView.default}/>
+          <Route path="/gift" component={Views.GiftView.default}/>
+          <Route path="/about" component={Views.AboutView.default}/>
+        </div>
+    </Router>
     )
   }
 }
