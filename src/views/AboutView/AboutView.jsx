cac0a0c1dced19dd298b45435094107ea64d0d02
hicoldcat <hicoldcat@foxmail.com>
2017-04-30 16:25:39 +0800

    add design and about views

---
+++ src/views/AboutView/AboutView.jsx
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class AboutView extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello,关于！</h1>
+    </div>
+    );
+  }
+}
---
+++ src/views/DesignView/DesignView.jsx
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class DesignView extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello,设计！</h1>
+    </div>
+    );
+  }
+}
