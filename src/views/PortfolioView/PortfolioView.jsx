8fce54c005c28a04e514bbf1196f0c0acd020076
hicoldcat <hicoldcat@foxmail.com>
2017-04-30 16:24:30 +0800

    add portfolio and traveler views

---
+++ src/views/PortfolioView/PortfolioView.jsx
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class PortfolioView extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello,作品！</h1>
+      </div>
+    );
+  }
+}
---
+++ src/views/TravelerView/TravelerView.jsx
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class TravelerView extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello,旅行！</h1>
+    </div>
+    );
+  }
+}
