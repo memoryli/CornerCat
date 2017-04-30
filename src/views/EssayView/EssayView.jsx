2e2b6eaf92194079ead2391af2f918c39a07ad33
hicoldcat <hicoldcat@foxmail.com>
2017-04-30 16:25:14 +0800

    add essay and gift views

---
+++ src/views/EssayView/EssayView.jsx
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class EssayView extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello,随笔！</h1>
+    </div>
+    );
+  }
+}
---
+++ src/views/GiftView/GiftView.jsx
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class GiftView extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello,好物！</h1>
+    </div>
+    );
+  }
+}
