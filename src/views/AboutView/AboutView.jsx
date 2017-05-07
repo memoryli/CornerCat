67dc0d8e92154ba35eaf465c151d7c133e5c89c6
hicoldcat <hicoldcat@foxmail.com>
2017-05-07 16:31:19 +0800

    design about view layout

--- src/views/AboutView/AboutView.jsx
+++ src/views/AboutView/AboutView.jsx
@@ -5,7 +5,39 @@ export default class AboutView extends Component {
   render() {
     return (
       <div className="about">
-        <h1>Hello,关于！</h1>
+
+        <div className="about-item-l">
+          <div className="about-left">
+            <h1 className="about-tit">Who Am  I ?</h1>
+            <p className="about-info">我希望是一个设计师，旅行作家，摄影师或者美食家。但我还不是......</p>
+          </div>
+          <div className="about-right">
+              <div className="about-img">
+                <img src="http://img.hb.aicdn.com/c12a049de6bdde4f3e737d263b638ce5b2c025e6b927f-IZdy6q_fw658"></img>
+              </div>
+              <div className="about-info">
+                信仰不灭
+              </div>
+          </div>
+        </div>
+
+        <div className="about-item-r">
+          <div className="about-left">
+            <div className="about-img">
+              <img src="http://img.hb.aicdn.com/c12a049de6bdde4f3e737d263b638ce5b2c025e6b927f-IZdy6q_fw658"></img>
+            </div>
+            <div className="about-info">
+              信仰不灭
+            </div>
+          </div>
+          <div className="about-right">
+
+              <h1 className="about-tit">What Do I Like ?</h1>
+              <p className="about-info">我是一个「伪文艺」「伪摄影」「伪旅行」的三伪爱好者！</p>
+          </div>
+        </div>
+
+
     </div>
     );
   }
