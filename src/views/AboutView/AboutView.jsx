779b22bab69b1b643469534dd34bf1060af07915
hicoldcat <hicoldcat@foxmail.com>
2017-05-09 12:59:52 +0800

    use state in about view

--- src/views/AboutView/AboutView.jsx
+++ src/views/AboutView/AboutView.jsx
@@ -3,47 +3,63 @@ import './AboutView.less'
 import axios from 'axios'
 
 export default class AboutView extends Component {
-  render() {
-    axios.get('/about').then((res) => {
-      console.info("Hello")
+  constructor(props) {
+    super(props);
+    this.state = {
+      list:[]
+    };
+  }
 
+  componentDidMount() {
+    axios.get('/api/about').then((res) => {
+      this.setState({
+        list:res.data.data.contentlist
+      })
     }).catch((error) => {
       console.warn(error)
     })
+  }
 
+  render() {
     return (
       <div className="about">
+        {
+          this.state.list.map(function (item,index) {
 
-        <div className="about-item-l">
-          <div className="about-left">
-            <h1 className="about-tit">Who Am  I ?</h1>
-            <p className="about-info">我希望是一个设计师，旅行作家，摄影师或者美食家。但我还不是......</p>
-          </div>
-          <div className="about-right">
-              <div className="about-img">
-                <img src="http://img.hb.aicdn.com/c12a049de6bdde4f3e737d263b638ce5b2c025e6b927f-IZdy6q_fw658"></img>
-              </div>
-              <div className="about-info">
-                信仰不灭
-              </div>
-          </div>
-        </div>
+            if (index%2 !== 0) {
+              return (
+                <div className="about-item-l"  key={item.id}>
+                  <div className="about-left">
+                    <h1 className="about-tit">{item.title}</h1>
+                    <p className="about-info">{item.info}</p>
+                  </div>
+                  <div className="about-right">
+                      <div className="about-img">
+                        <img src={item.imgurl}></img>
+                      </div>
+                      <div className="about-info">{item.imgdesc}</div>
+                  </div>
+                </div>
+              )
+            } else {
+              return (
+                <div className="about-item-r" key={item.id}>
+                  <div className="about-left">
+                    <div className="about-img">
+                      <img src={item.imgurl}></img>
+                    </div>
+                    <div className="about-info">{item.imgdesc}</div>
+                  </div>
+                  <div className="about-right">
+                      <h1 className="about-tit">{item.title}</h1>
+                      <p className="about-info">{item.info}</p>
+                  </div>
+                </div>
+              )
+            }
 
-        <div className="about-item-r">
-          <div className="about-left">
-            <div className="about-img">
-              <img src="http://cdn.duitang.com/uploads/item/201208/17/20120817205028_PBcHP.thumb.600_0.jpeg"></img>
-            </div>
-            <div className="about-info">
-              爱就去追
-            </div>
-          </div>
-          <div className="about-right">
-              <h1 className="about-tit">What Do I Like ?</h1>
-              <p className="about-info">我是一个「伪文艺」「伪摄影」「伪旅行」的三伪爱好者！</p>
-          </div>
-        </div>
-
+          })
+        }
 
     </div>
     );
