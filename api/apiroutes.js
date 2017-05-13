669af44c3898e62963465e775ae699da59ff54b1
hicoldcat <hicoldcat@foxmail.com>
2017-05-13 13:01:02 +0800

    add portfolio json

--- api/apiroutes.js
+++ api/apiroutes.js
@@ -1,13 +1,16 @@
 var express = require('express')
 var apiRoutes = express.Router()
 
-// var portfolio = require('./mockdata/portfolio.json')
+var portfolio = require('./mockdata/portfolio.json')
 // var traveler = require('./mockdata/traveler.json')
 // var essay = require('./mockdata/essay.json')
 // var design = require('./mockdata/design.json')
 // var gift = require('./mockdata/gift.json')
 var about = require('./mockdata/about.json')
 
+apiRoutes.get('/portfolio', function (req, res) {
+	res.json(portfolio);
+});
 
 apiRoutes.get('/about', function (req, res) {
 	res.json(about);
