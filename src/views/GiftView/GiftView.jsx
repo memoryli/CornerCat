8fb67f6131f32fc64e065472e39610ba8da0fa2f
hicoldcat <hicoldcat@foxmail.com>
2017-05-21 13:59:26 +0800

    design giftview layout

--- src/views/GiftView/GiftView.jsx
+++ src/views/GiftView/GiftView.jsx
@@ -5,7 +5,33 @@ export default class GiftView extends Component {
   render() {
     return (
       <div className="gift">
-        <h1>Hello,好物！</h1>
+        <ul className="gift-list">
+          <li className="gift-list-item">
+            <img src="http://img.hb.aicdn.com/8b92aded76111813a77bb56ed1ab89df5961766a53e8e-pZjx14_fw658" ></img>
+          </li>
+          <li className="gift-list-item">
+            <img src="http://img.hb.aicdn.com/7a479e45c373073a9867c573d049b902969a1a506c690-TFAlrV_fw658" ></img>
+          </li>
+          <li className="gift-list-item">
+            <img src="http://img.hb.aicdn.com/f5e4f15a230eb0fe499c5b1acd1e000c58d592e917632-K2pzIW_fw658" ></img>
+          </li>
+          <li className="gift-list-item">
+            <img src="http://img.hb.aicdn.com/d3b5897936790991033f8b632b7695718b27c9f0ddef-QlG98D_fw658" ></img>
+            <p>Hell Hell Hell Hell Hell Hell HellHell Hell Hell Hell Hell Hell HellHell Hell Hell Hell Hell Hell HellHell Hell Hell Hell Hell Hell HellHell Hell Hell Hell Hell Hell Hell</p>
+          </li>
+          <li className="gift-list-item">
+            <img src="http://img.hb.aicdn.com/96cbb015ab9815fb8b525eb853d6840e188e1b4f94a66-LpNCvQ_fw658" ></img>
+          </li>
+          <li className="gift-list-item">
+            <img src="http://img.hb.aicdn.com/24ae038ad28790afa292fa7f449e314468524bfe32737-Vi6Tl8_fw658" ></img>
+          </li>
+          <li className="gift-list-item">
+            <img src="http://img.hb.aicdn.com/247ced4a6379e5d9f5a1445942d8c3e01ac0c8623bbd3-Qx1cRU_fw658" ></img>
+          </li>
+          <li className="gift-list-item">
+            <img src="http://img.hb.aicdn.com/deb733a37fc3bc2485aeff09667cb88530383b4a2b194-cVnOmG_fw658" ></img>
+          </li>
+        </ul>
     </div>
     );
   }
