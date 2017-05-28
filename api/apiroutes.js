6d22825947b5a3578e16f03abb6276909b824b7d
hicoldcat <hicoldcat@foxmail.com>
2017-05-28 16:27:42 +0800

    add essay view  mock data

--- api/apiroutes.js
+++ api/apiroutes.js
@@ -3,13 +3,17 @@ var apiRoutes = express.Router()
 
 var portfolio = require('./mockdata/portfolio.json')
 // var traveler = require('./mockdata/traveler.json')
-// var essay = require('./mockdata/essay.json')
+var essay = require('./mockdata/essay.json')
 // var design = require('./mockdata/design.json')
 var gift = require('./mockdata/gift.json')
 var about = require('./mockdata/about.json')
 
 apiRoutes.get('/portfolio', function (req, res) {
 	res.json(portfolio);
+});
+
+apiRoutes.get('/essay', function (req, res) {
+	res.json(essay);
 });
 
 apiRoutes.get('/gift', function (req, res) {
