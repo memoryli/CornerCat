8886c3df6dee7c238400f8c9ca043cf26a7ffca3
hicoldcat <hicoldcat@foxmail.com>
2017-05-28 16:29:06 +0800

    add essayview style

--- src/views/EssayView/EssayView.jsx
+++ src/views/EssayView/EssayView.jsx
@@ -1,11 +1,52 @@
 import React, { Component, PropTypes } from 'react'
 import './EssayView.less'
+import axios from 'axios'
+
+import TimeBox from 'components/TimeBox/TimeBox'
 
 export default class EssayView extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      list:[]
+    };
+  }
+
+  componentWillMount() {
+    axios.get('/api/essay').then((res) => {
+      this.setState({
+        list:res.data.data
+      })
+      console.info(res.data.data)
+    }).catch((error) => {
+      console.warn(error)
+    })
+  }
+
+
+
   render() {
     return (
       <div className="essay">
-        <h1>Hello,随笔！</h1>
+        <div className="list">
+          {
+            this.state.list.map(function(item,index){
+              return (
+                <div className="item" key={item.id}>
+                  <div className="cover">
+                    <img src={item.cover}></img>
+                  </div>
+                  <div className="info">{item.info}</div>
+                  <div className="status">
+                    <TimeBox time={item.createtime} />
+                    <div className="author"><span>by:</span>{item.author}</div>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
     </div>
     );
   }
